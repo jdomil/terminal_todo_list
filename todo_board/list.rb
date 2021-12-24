45fbea4867e4b94cae5b3ceb2849c82e3af7107b
juan <juan@stripe.com>
2021-12-24 20:24:45 +0100

    Integrating print and item moving functionality

--- todo_board/list.rb
+++ todo_board/list.rb
@@ -41,4 +41,59 @@ class List
   def priority
     @items[0]
   end
+
+  def print
+    puts '------------------------------------------'
+    puts 'GROCERIES'.center(42)
+    puts '------------------------------------------'
+    puts 'Index'.ljust(6) + '| Item'.ljust(23) + '| Deadline'.ljust(13)
+    puts '------------------------------------------'
+
+    @items.each_with_index do |item, idx|
+      puts idx.to_s.ljust(6) + "| #{item.title}".ljust(23) + "| #{item.deadline}".ljust(13)
+    end
+
+    puts '------------------------------------------'
+  end
+
+  def print_full_item(index)
+    return false unless valid_index?(index)
+
+    puts '------------------------------------------'
+    puts @items[index].title.to_s.ljust(21) + @items[index].deadline.to_s.rjust(21)
+    puts @items[index].description.to_s.ljust(42)
+    puts '------------------------------------------'
+    true
+  end
+
+  def print_priority
+    puts '------------------------------------------'
+    puts @items[0].title.to_s.ljust(21) + @items[0].deadline.to_s.rjust(21)
+    puts @items[0].description.to_s.ljust(42)
+    puts '------------------------------------------'
+  end
+
+  def up(index, amount = 1)
+    return false unless valid_index?(index)
+
+    amount.times do
+      break if (index - 1).negative?
+
+      @items[index], @items[index - 1] = @items[index - 1], @items[index]
+      index -= 1
+    end
+    true
+  end
+
+  def down(index, amount = 1)
+    return false unless valid_index?(index)
+
+    amount.times do
+      break if (index + 1) >= @items.size
+
+      @items[index], @items[index + 1] = @items[index + 1], @items[index]
+      index += 1
+    end
+    true
+  end
 end
